d198f319bb46689c042ffaa6962703e1f75dd35e
wangbigan <wangbigan123@126.com>
2022-09-20 23:16:19 +0800

    1. 将条件改为距离中线不超过0.6的赛道宽度，让小车在过弯是选择空间更大，灵活度更高

--- reward_function.py
+++ reward_function.py
@@ -1,22 +1,23 @@
 # 基于三个基本原则
-# 1. 尽量在靠近中线的位置或者跟中线平行的状态前进
-# 2. 尽可能走直线
-# 3. 尽可能提高完成度
-# 4. 在1/2/3基础上，尽可能提高车速
+# 1. 尽可能与中线平行行驶
+# 2. 尽可能靠近中线
+# 3. 尽可能提高车速
+# 4. 尽可能提高完成度
+
 
 def reward_function(params):
     reward = 0.0
 
     # Read input parameters
+    track_width = params['track_width']
     distance_from_center = params['distance_from_center']
     speed = params['speed']
     abs_steering = abs(params['steering_angle'])
 
-    reward += 1 / (abs_steering / 100 + 1)  # 角度越小，奖励越高，角度为零和在中心线上行驶奖励同时最大化
-    reward += 1 / (distance_from_center + 1)  # 越靠近中心，奖励越高
-    reward += (speed / 2)
-
-    if params['all_wheels_on_track']:
+    if distance_from_center < 0.6 * track_width:
+        reward += 1 / (abs_steering / 100 + 1)  # 角度越小，奖励越高，角度为零和在中心线上行驶奖励同时最大化
+        reward += 1 / (distance_from_center + 1)  # 越靠近中心，奖励越高
+        reward += (speed / 2)  # 速度越快，奖励越高
         reward += (params['progress'] / 2)  # 完成度越高，奖励越高
 
     return float(reward)
